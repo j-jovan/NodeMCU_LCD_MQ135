c92cc111df1b883ad26f36472c09d48765d038e6
j-jovan <abc.j.jovan@gmail.com>
2019-08-27 11:41:27 +0200

    Working WiFi, LCD and Smoke sensor

--- NodeMCU_LCD_MQ135.ino
+++ NodeMCU_LCD_MQ135.ino
@@ -1,15 +1,50 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
+/* Put your SSID & Password */
+const char* ssid = "NodeMCU";  // Enter SSID here
+const char* password = "12345678";  //Enter Password here
+
+
+/* Put IP Address details */
+IPAddress local_ip(192, 168, 1, 1);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+ESP8266WebServer server(80);
+
+uint8_t LED1pin = D8;
+bool LED1status = HIGH;
+
+uint8_t LED2pin = D7;
+bool LED2status = LOW;
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 int sensorValue;
 int digitalValue;
 
-void setup() {
 
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(3, INPUT);
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+
+
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+
+  server.on("/", handle_OnConnect);
+  server.on("/led1on", handle_led1on);
+  server.on("/led1off", handle_led1off);
+  server.on("/led2on", handle_led2on);
+  server.on("/led2off", handle_led2off);
+  server.onNotFound(handle_NotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
 
   Wire.begin(2, 0);
   lcd.begin();
@@ -18,9 +53,74 @@ void setup() {
   delay(5000);
 }
 void loop() {
+  server.handleClient();
   sensorValue = analogRead(0);
   Wire.begin(2, 0);
   lcd.begin();
   lcd.backlight();
-  lcd.print(sensorValue);
+
+  if (sensorValue < 100) {
+    lcd.print("Nema dima");
+    digitalWrite(LED1pin, LOW);
+  }
+
+  if (sensorValue > 100) {
+    lcd.print("DIM");
+    digitalWrite(LED1pin, HIGH);
+  }
+
+}
+
+void handle_OnConnect() {
+  LED1status = LOW;
+  LED2status = LOW;
+  Serial.println("GPIO7 Status: OFF | GPIO6 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status, LED2status));
+}
+
+void handle_led1on() {
+  LED1status = HIGH;
+  Serial.println("GPIO7 Status: ON");
+  server.send(200, "text/html", SendHTML(true, LED2status));
+}
+
+void handle_led1off() {
+  LED1status = LOW;
+  Serial.println("GPIO7 Status: OFF");
+  server.send(200, "text/html", SendHTML(false, LED2status));
+}
+
+void handle_led2on() {
+  LED2status = HIGH;
+  Serial.println("GPIO6 Status: ON");
+  server.send(200, "text/html", SendHTML(LED1status, true));
+}
+
+void handle_led2off() {
+  LED2status = LOW;
+  Serial.println("GPIO6 Status: OFF");
+  server.send(200, "text/html", SendHTML(LED1status, false));
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(uint8_t led1stat, uint8_t led2stat) {
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>LED Control</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<h1>Sensor dima:</h1>\n";
+  ptr += sensorValue;
+  ptr += "\n";
+
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
 }
