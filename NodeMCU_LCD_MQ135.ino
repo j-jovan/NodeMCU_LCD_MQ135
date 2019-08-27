a5c59c372b6d1a357e9191855a9cd28a102efd1e
j-jovan <abc.j.jovan@gmail.com>
2019-08-27 09:18:46 +0200

    Working LCD and sensor

--- NodeMCU_LCD_MQ135.ino
+++ NodeMCU_LCD_MQ135.ino
@@ -1,14 +1,26 @@
 #include <Wire.h>
-
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-void setup(){
-  Wire.begin(2,0);
-  lcd.begin();   
-  lcd.backlight();  
-  lcd.print("Test"); 
-}
-void loop(){
+int sensorValue;
+int digitalValue;
 
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(3, INPUT);
+
+  Wire.begin(2, 0);
+  lcd.begin();
+  lcd.backlight();
+  lcd.print("LCD radi");
+  delay(5000);
+}
+void loop() {
+  sensorValue = analogRead(0);
+  Wire.begin(2, 0);
+  lcd.begin();
+  lcd.backlight();
+  lcd.print(sensorValue);
 }
