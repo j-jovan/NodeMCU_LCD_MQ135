c8668ac2e268e8956f0c01cd168fda86ebadbe39
j-jovan <abc.j.jovan@gmail.com>
2019-08-27 08:46:33 +0200

    Working display

--- NodeMCU_LCD_MQ135.ino
+++ NodeMCU_LCD_MQ135.ino
@@ -1,1 +1,14 @@
+#include <Wire.h>
 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+void setup(){
+  Wire.begin(2,0);
+  lcd.begin();   
+  lcd.backlight();  
+  lcd.print("Test"); 
+}
+void loop(){
+
+}
